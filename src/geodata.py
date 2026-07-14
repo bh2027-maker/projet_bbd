2ec37858088af6940059d4dd03b75131e9eb66de
bh2027-maker <mohammedcherifcf@gmail.com>
2026-07-14 18:07:21 +0200

    Add function to enrich data with INSEE information
    Added a function to merge local data with INSEE dataset.

--- src/geodata.py
+++ src/geodata.py
@@ -22,6 +22,19 @@ def enrichir_communes(df):
     df['nb_logements'] = 0
     return df
 
+
+def enrichir_avec_insee(df_bauges, path_insee_csv):
+    """
+    Fusionne la base locale avec le dataset global INSEE
+    path_insee_csv : chemin vers le fichier complet INSEE
+    """
+    df_insee = pd.read_csv(path_insee_csv, sep=';', dtype={'CODGEO': str})
+    
+    # On fait une jointure (Merge) sur le code commune
+    df_final = pd.merge(df_bauges, df_insee, left_on='code_insee', right_on='CODGEO', how='left')
+    
+    return df_final
+
 if __name__ == "__main__":
     data = load_communes()
     if data is not None:
